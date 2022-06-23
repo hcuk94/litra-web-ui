46d7953e7583285dce8adbf1b7037aa66a84adcb
hcuk94 <me@hc.me.uk>
2022-06-23 23:02:33 +0100

    some more error handling

--- main.go
+++ main.go
@@ -19,18 +19,26 @@ func main() {
 }
 
 func apiOn(c *gin.Context) {
-	ld, _ := litra.New()
-	ld.TurnOn()
-	ld.Close()
-	c.Status(200)
+	ld, err := litra.New()
+	if err != nil {
+		c.AbortWithStatus(500)
+	} else {
+		ld.TurnOn()
+		ld.Close()
+		c.Status(200)
+	}
 	return
 }
 
 func apiOff(c *gin.Context) {
-	ld, _ := litra.New()
-	ld.TurnOff()
-	ld.Close()
-	c.Status(200)
+	ld, err := litra.New()
+	if err != nil {
+		c.AbortWithStatus(500)
+	} else {
+		ld.TurnOff()
+		ld.Close()
+		c.Status(200)
+	}
 	return
 }
 
@@ -38,9 +46,12 @@ func apiBrightness(c *gin.Context) {
 	param, _ := strconv.Atoi(c.Param("val"))
 	val := int(param)
 	ld, err := litra.New()
-	if err == nil {
+	if err != nil {
+		c.AbortWithStatus(500)
+	} else {
 		ld.SetBrightness(val)
 		ld.Close()
+		c.Status(200)
 	}
 }
 
@@ -48,8 +59,11 @@ func apiTemperature(c *gin.Context) {
 	param, _ := strconv.Atoi(c.Param("val"))
 	val := int16(param)
 	ld, err := litra.New()
-	if err == nil {
+	if err != nil {
+		c.AbortWithStatus(500)
+	} else {
 		ld.SetTemperature(val)
 		ld.Close()
+		c.Status(200)
 	}
 }
