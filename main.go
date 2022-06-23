d0f36f7d9c3426c10d755ddca9721a1c1039f5d6
hcuk94 <me@hc.me.uk>
2022-06-24 00:23:59 +0100

    some rehashing of main func

--- main.go
+++ main.go
@@ -7,63 +7,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var ld *litra.LitraDevice
+
 func main() {
-	router := gin.Default()
-	// Define API Endpoints
-	router.GET("/on", apiOn)
-	router.GET("/off", apiOff)
-	router.GET("/brightness/:val", apiBrightness)
-	router.GET("/temperature/:val", apiTemperature)
-	// Run the Web Server
-	router.Run(":8080")
+	// Initialise Litra Device
+	var ld_err error
+	ld, ld_err = litra.New()
+	if ld_err == nil {
+		// Define Gin Router
+		router := gin.Default()
+		// Define API Endpoints
+		router.GET("/on", apiOn)
+		router.GET("/off", apiOff)
+		router.GET("/brightness/:val", apiBrightness)
+		router.GET("/temperature/:val", apiTemperature)
+		// Run the Web Server
+		router.Run(":8080")
+	}
+	// Close Litra Device
+	ld.Close()
 }
 
 func apiOn(c *gin.Context) {
-	ld, err := litra.New()
-	if err != nil {
-		c.AbortWithStatus(500)
-	} else {
-		ld.TurnOn()
-		ld.Close()
-		c.Status(200)
-	}
+	ld.TurnOn()
+	c.Status(200)
 	return
 }
 
 func apiOff(c *gin.Context) {
-	ld, err := litra.New()
-	if err != nil {
-		c.AbortWithStatus(500)
-	} else {
-		ld.TurnOff()
-		ld.Close()
-		c.Status(200)
-	}
+	ld.TurnOff()
+	c.Status(200)
 	return
 }
 
 func apiBrightness(c *gin.Context) {
 	param, _ := strconv.Atoi(c.Param("val"))
 	val := int(param)
-	ld, err := litra.New()
-	if err != nil {
-		c.AbortWithStatus(500)
-	} else {
-		ld.SetBrightness(val)
-		ld.Close()
-		c.Status(200)
-	}
+	ld.SetBrightness(val)
+	c.Status(200)
+	return
 }
 
 func apiTemperature(c *gin.Context) {
 	param, _ := strconv.Atoi(c.Param("val"))
 	val := int16(param)
-	ld, err := litra.New()
-	if err != nil {
-		c.AbortWithStatus(500)
-	} else {
-		ld.SetTemperature(val)
-		ld.Close()
-		c.Status(200)
-	}
+	ld.SetTemperature(val)
+	c.Status(200)
+	return
 }
