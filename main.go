26a2fb0e77697ecc9f588f27b5bba74a2b634d96
hcuk94 <me@hc.me.uk>
2022-06-23 22:46:00 +0100

    various updates

--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"strconv"
 
 	"github.com/derickr/go-litra-driver"
+	"github.com/gin-gonic/gin"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, world!</h1>")
+func main() {
+	router := gin.Default()
+	// Define API Endpoints
+	router.GET("/on", apiOn)
+	router.GET("/off", apiOff)
+	router.GET("/brightness/:val", apiBrightness)
+	router.GET("/temperature/:val", apiTemperature)
+	// Run the Web Server
+	router.Run(":8080")
 }
 
-func main() {
-
+func apiOn(c *gin.Context) {
 	ld, err := litra.New()
-	if err != nil {
-		fmt.Println("I am sad :-(")
+	if err == nil {
+		ld.TurnOn()
+		ld.Close()
 	}
-	ld.TurnOn()
-	ld.Close()
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting server...")
-	http.ListenAndServe(":8080", nil)
+}
+
+func apiOff(c *gin.Context) {
+	ld, err := litra.New()
+	if err == nil {
+		ld.TurnOn()
+		ld.Close()
+	}
+}
+
+func apiBrightness(c *gin.Context) {
+	param, _ := strconv.Atoi(c.Param("val"))
+	val := int(param)
+	ld, err := litra.New()
+	if err == nil {
+		ld.SetBrightness(val)
+		ld.Close()
+	}
+}
+
+func apiTemperature(c *gin.Context) {
+	param, _ := strconv.Atoi(c.Param("val"))
+	val := int16(param)
+	ld, err := litra.New()
+	if err == nil {
+		ld.SetTemperature(val)
+		ld.Close()
+	}
 }
